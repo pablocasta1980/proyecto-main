69a690235fc3f8222ee5eb391e12086fc33bce7a
pablocasta1980 <pacastanog@uqvirtual.edu.co>
2025-05-21 23:31:31 -0500

    actualizacion

--- src/main/java/co/edu/uniquindio/proyecto/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto.config;
 
+
 import co.edu.uniquindio.proyecto.seguridad.AutenticacionEntryPoint;
 import co.edu.uniquindio.proyecto.seguridad.JWTFilter;
 import lombok.RequiredArgsConstructor;
@@ -20,58 +21,84 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+
 import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+
     private final JWTFilter jwtFilter;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Configura la seguridad HTTP para la aplicación
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
-                        // Configuración de permisos para endpoints públicos
+                        // Rutas públicas
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/imagenes").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/recuperarPassword").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/cambiar-password").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/registro").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/activar/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/reenviar-token").permitAll()
                         .requestMatchers(
-                                "/api/auth",
-                                "/api/imagenes",
-                                "/api/usuarios/registro",
-                                "/api/usuarios/activar/**",
-                                "/api/usuarios/reenviar-token",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/api/reportes/**",
-                                "/api/categorias/**",
-                                "/api/comentarios/**"
-                        ).permitAll()
+                                "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/reportes").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/cercanos").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/buscar").permitAll()
 
-                        // Configuración de roles y permisos específicos
-                        .requestMatchers(HttpMethod.POST, "/api/usuarios/notificaciones/suscribirse").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/actualizar-password").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/**").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.POST, "/api/reportes").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.PUT, "/api/reportes/{id}").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.DELETE, "/api/reportes/{id}").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.POST, "/api/reportes/{id}/importante").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.POST, "/api/reportes/{id}/calificacion").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.GET, "/api/reportes/mis-reportes").hasAuthority("CLIENTE")
-                        .requestMatchers(HttpMethod.POST, "/api/comentarios/**").hasAuthority("CLIENTE")
 
-                        .requestMatchers(HttpMethod.POST, "/api/categorias").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PUT, "/api/categorias/{id}").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.DELETE, "/api/categorias/{id}").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/reportes/informe").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/reportes/filtrar").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/usuarios").hasAuthority("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.GET, "/api/categorias/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/categorias").permitAll()
 
+                        .requestMatchers(HttpMethod.GET, "/api/comentarios/**").permitAll()
+
+                        //Se establecen los permisos en las rutas de backend
+                        .requestMatchers(HttpMethod.GET, "/api/usuarios/{id}").permitAll()
+
+
+                        // Rutas solo para CLIENTES autenticados
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/notificaciones/suscribirse").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/actualizar-password").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/**").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasAuthority("ROLE_CLIENTE")
+
+                        .requestMatchers(HttpMethod.POST, "/api/reportes").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.PUT, "/api/reportes/{id}").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.DELETE, "/api/reportes/{id}").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/api/reportes/{id}/importante").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/api/reportes/{id}/calificacion").hasAuthority("ROLE_CLIENTE")
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/mis-reportes").hasAuthority("ROLE_CLIENTE")
+
+                        .requestMatchers(HttpMethod.POST, "/api/comentarios/**").hasAuthority("ROLE_CLIENTE")
+
+
+                        // Rutas solo para ADMINISTRADORES
+                        .requestMatchers(HttpMethod.POST, "/api/categorias").hasAuthority("ROLE_ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/categorias/{id}").hasAuthority("ROLE_ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/categorias/{id}").hasAuthority("ROLE_ADMINISTRADOR")
+
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/informe").hasAuthority("ROLE_ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.GET, "/api/reportes/filtrar").hasAuthority("ROLE_ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.GET, "/api/usuarios").hasAuthority("ROLE_ADMINISTRADOR")
+
+                        // Ruta administradores y clientes
+                        .requestMatchers(HttpMethod.PUT, "/api/reportes/{id}/estado").permitAll()
+
+
+                        // Todo lo demás requiere autenticación
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(new AutenticacionEntryPoint()))
@@ -80,45 +107,35 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        // Configura las políticas de CORS para permitir solicitudes desde el frontend
         CorsConfiguration config = new CorsConfiguration();
-
-        // Configuración CORS mejorada
-        config.setAllowedOrigins(Arrays.asList(
-                "http://localhost:4200",
-                "https://alertascomunitariasapp.firebaseapp.com",
-                "https://alertascomunitariasapp.web.app",
-                "https://proyecto-main-3vcz.onrender.com"
-        ));
-
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(Arrays.asList(
-                "Authorization",
-                "Cache-Control",
-                "Content-Type",
-                "Access-Control-Allow-Origin",
-                "X-Requested-With"
-        ));
-        config.setExposedHeaders(Arrays.asList(
-                "Authorization",
-                "Content-Disposition"
-        ));
+        config.setAllowedOrigins(List.of("*"));
+        //Permitir Comunicacion con Angular
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
+        // Permite codificar y verificar contraseñas utilizando BCrypt
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        // Proporciona un AuthenticationManager para la autenticación de usuarios
         return configuration.getAuthenticationManager();
     }
 }
