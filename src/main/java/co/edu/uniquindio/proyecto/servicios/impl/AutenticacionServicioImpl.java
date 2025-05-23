07582e3834f8b62b8d8f9928872942b6f86f953b
YarleyMejia <100544029+YarleyMejia@users.noreply.github.com>
2025-05-22 20:57:33 -0500

    Revert "correccion en la implementacion de recuperar contraseña"
    This reverts commit 76962355ff55b7ac4fd56792e84a227075d31b7e.

--- src/main/java/co/edu/uniquindio/proyecto/servicios/impl/AutenticacionServicioImpl.java
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/impl/AutenticacionServicioImpl.java
@@ -87,9 +87,9 @@ public class AutenticacionServicioImpl  implements AutenticacionServicio {
         String tokenRecuperacion = jwtUtils.generateToken(usuario.getEmail(), claims, 15); // 15 minutos
 
         // Crear el contenido del correo
-        String cuerpoCorreo = "Usa el siguiente enlace o código para recuperar tu cuenta:\n\n"
+        String cuerpoCorreo = "Copia el siguiente código para recuperar tu cuenta:\n\n"
                 + tokenRecuperacion + "\n\n"
-                + "Este token expirará en 15 minutos.";
+                + "Token expira en 15 minutos.";
 
         EmailDTO emailDTO = new EmailDTO(
                 usuario.getEmail(),
